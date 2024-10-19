cbc3864d68eb699917edd70d00774c89970a3b53
PT Spekta Karya Raya <spektakaryaraya@gmail.com>
2024-10-19 08:34:46 +0700

    feat: add theme for admin panel

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,25 @@
-import defaultTheme from 'tailwindcss/defaultTheme';
-import forms from '@tailwindcss/forms';
-import typography from '@tailwindcss/typography';
+import defaultTheme from "tailwindcss/defaultTheme";
+import preset from "./vendor/filament/support/tailwind.config.preset";
 
 /** @type {import('tailwindcss').Config} */
 export default {
+    presets: [preset],
     content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './vendor/laravel/jetstream/**/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
+        "./app/Filament/**/*.php",
+        "./lang/**/*.php",
+        "./resources/views/**/*.blade.php",
+        "./resources/js/**/*.js",
+        "./storage/framework/views/*.php",
+        "./vendor/filament/**/*.blade.php",
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+        "./vendor/laravel/jetstream/**/*.blade.php",
     ],
 
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                sans: ["Figtree", ...defaultTheme.fontFamily.sans],
             },
         },
     },
-
-    plugins: [forms, typography],
 };
