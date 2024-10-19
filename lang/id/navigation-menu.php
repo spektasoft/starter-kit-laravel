5269f45541945c8d4e4b4b99a123df06f399cde9
PT Spekta Karya Raya <spektakaryaraya@gmail.com>
2024-10-19 12:51:42 +0700

    feat: add language switcher

---
+++ lang/id/navigation-menu.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'language_switcher' => [
+        'language' => 'Bahasa',
+        'open_language_switcher' => 'Buka Pengubah Bahasa',
+    ],
+];
