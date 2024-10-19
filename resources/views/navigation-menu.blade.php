726da42474da360fdeea56029ad56aaa388580c9
PT Spekta Karya Raya <spektakaryaraya@gmail.com>
2024-10-19 20:19:13 +0700

    feat: add wire navigation for navigation items

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -28,7 +28,7 @@
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link href="{{ route('home') }}" :active="request()->routeIs('home')">
+                    <x-nav-link wire:navigate href="{{ route('home') }}" :active="request()->routeIs('home')">
                         {{ __('navigation-menu.menu.home') }}
                     </x-nav-link>
                 </div>
@@ -56,7 +56,7 @@
     <!-- Responsive Navigation Menu -->
     <div :class="{ 'block': open, 'hidden': !open }" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
-            <x-responsive-nav-link href="{{ route('home') }}" :active="request()->routeIs('home')">
+            <x-responsive-nav-link wire:navigate href="{{ route('home') }}" :active="request()->routeIs('home')">
                 {{ __('navigation-menu.menu.home') }}
             </x-responsive-nav-link>
         </div>
