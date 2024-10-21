5a4c7c242eabb3801679590e4a9838733ce456fb
PT Spekta Karya Raya <spektakaryaraya@gmail.com>
2024-10-21 16:24:51 +0700

    feat: add login api endpoint

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,17 @@
 <?php
 
+use App\Http\Controllers\API\V1\LoginController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::group(['prefix' => 'v1'], function () {
+    Route::post('/login', LoginController::class)
+        ->middleware('guest:'.config('fortify.guard'))
+        ->name('api.v1.login');
+
+    Route::group(['middleware' => 'auth:sanctum'], function () {
+        Route::get('/user', function (Request $request) {
+            return $request->user();
+        })->name('api.v1.user');
+    });
+});
