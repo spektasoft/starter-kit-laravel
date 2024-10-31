4351b9cfdc4f31136eaea55bf7846e5425f9ec72
PT Spekta Karya Raya <spektakaryaraya@gmail.com>
2024-10-31 09:05:14 +0700

    test: update authentication and login test

--- tests/Feature/Livewire/Auth/LoginTest.php
+++ tests/Feature/Livewire/Auth/LoginTest.php
@@ -3,14 +3,26 @@
 namespace Tests\Feature\Livewire\Auth;
 
 use App\Livewire\Auth\Login;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Livewire\Livewire;
 use Tests\TestCase;
 
 class LoginTest extends TestCase
 {
+    use RefreshDatabase;
+
     public function test_login_can_be_rendered(): void
     {
         Livewire::test(Login::class)
             ->assertStatus(200);
+    }
+
+    public function test_login_has_form_and_fields(): void
+    {
+        Livewire::test(Login::class)
+            ->assertFormExists()
+            ->assertFormFieldExists('email')
+            ->assertFormFieldExists('password')
+            ->assertFormFieldExists('remember');
     }
 }
