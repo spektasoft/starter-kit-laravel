5d89a931282b8aea0ae827b28f4f344f0ae886e6
farshazizi <farshazizi22@gmail.com>
2024-10-31 23:16:57 +0700

    Created model

---
+++ app/Models/Permission.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUlids;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+
+class Permission extends Model
+{
+    use HasUlids;
+
+    protected static function booted()
+    {
+        static::creating(function ($model) {
+            if (empty($model->id)) {
+                $model->id = Str::ulid();
+            }
+        });
+    }
+}
