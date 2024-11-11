31d9cfd08ee659b05be1e450d8a1ecee64658b5d
PT Spekta Karya Raya <spektakaryaraya@gmail.com>
2024-11-11 12:25:18 +0700

    feat: add user policy

---
+++ app/Policies/UserPolicy.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class UserPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Determine whether the user can view any models.
+     */
+    public function viewAny(User $user): bool
+    {
+        return $user->can('view_any_user');
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     */
+    public function view(User $user, User $otherUser): bool
+    {
+        return $user->can('view_user');
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     */
+    public function viewAll(User $user): bool
+    {
+        return $user->can('view_all_user');
+    }
+
+    /**
+     * Determine whether the user can create models.
+     */
+    public function create(User $user): bool
+    {
+        return $user->can('create_user');
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     */
+    public function update(User $user, User $otherUser): bool
+    {
+        return $user->can('update_user');
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     */
+    public function delete(User $user, User $otherUser): bool
+    {
+        return $user->can('delete_user');
+    }
+
+    /**
+     * Determine whether the user can bulk delete.
+     */
+    public function deleteAny(User $user): bool
+    {
+        return $user->can('delete_any_user');
+    }
+
+    /**
+     * Determine whether the user can permanently delete.
+     */
+    public function forceDelete(User $user, User $otherUser): bool
+    {
+        return $user->can('force_delete_user');
+    }
+
+    /**
+     * Determine whether the user can permanently bulk delete.
+     */
+    public function forceDeleteAny(User $user): bool
+    {
+        return $user->can('force_delete_any_user');
+    }
+
+    /**
+     * Determine whether the user can restore.
+     */
+    public function restore(User $user, User $otherUser): bool
+    {
+        return $user->can('restore_user');
+    }
+
+    /**
+     * Determine whether the user can bulk restore.
+     */
+    public function restoreAny(User $user): bool
+    {
+        return $user->can('restore_any_user');
+    }
+
+    /**
+     * Determine whether the user can replicate.
+     */
+    public function replicate(User $user, User $otherUser): bool
+    {
+        return $user->can('replicate_user');
+    }
+
+    /**
+     * Determine whether the user can reorder.
+     */
+    public function reorder(User $user): bool
+    {
+        return $user->can('reorder_user');
+    }
+}
