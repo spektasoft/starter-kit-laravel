154c832720f11c6da63a60ca9b0b55450ce8cbda
PT Spekta Karya Raya <spektakaryaraya@gmail.com>
2024-11-23 14:43:32 +0700

    chore: update project

--- app/Models/User.php
+++ app/Models/User.php
@@ -40,6 +40,8 @@ class User extends Authenticatable implements FilamentUser, HasAvatar, MustVerif
 
     /**
      * The attributes that are mass assignable.
+     *
+     * @var list<string>
      */
     protected $fillable = [
         'profile_photo_media_id',
@@ -50,6 +52,8 @@ class User extends Authenticatable implements FilamentUser, HasAvatar, MustVerif
 
     /**
      * The attributes that should be hidden for serialization.
+     *
+     * @var list<string>
      */
     protected $hidden = [
         'password',
