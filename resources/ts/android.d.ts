7bed6d7fdd93bb5cd74a7c810af0ccaf686f7404
PT Spekta Karya Raya <spektakaryaraya@gmail.com>
2025-02-23 23:37:33 +0700

    feat: add language switcher interface

---
+++ resources/ts/android.d.ts
@@ -0,0 +1,3 @@
+export interface Android {
+    switchLanguage(lang: string): void;
+}
--- resources/ts/global.d.ts
+++ resources/ts/global.d.ts
@@ -2,12 +2,14 @@ import { Alpine } from "alpinejs";
 import { AxiosStatic } from "axios";
 // @ts-ignore
 import { Livewire } from "../../vendor/livewire/livewire/dist/livewire.js";
+import { Android } from "./android";
 
 declare global {
     interface Window {
         Alpine: Alpine;
         axios: AxiosStatic;
         Livewire: Livewire;
+        Android?: Android;
     }
 }
 
