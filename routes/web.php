a37b92754bc9bbae240076cf7072bffe2762f37d
PT Spekta Karya Raya <spektakaryaraya@gmail.com>
2025-03-24 23:05:47 +0700

    feat: add Terms of Service and Privacy Policy links
    - Deleted obsolete Markdown files related to Terms of Service and Privacy Policy.
    - Added new pages for viewing Terms of Service and Privacy Policy in the corresponding Blade templates.
    - Updated web routes to include links for Terms of Service and Privacy Policy.

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Models\Page;
 use Illuminate\Support\Facades\Route;
+use Laravel\Jetstream\Jetstream;
 
 Route::get('/', function () {
     return view('welcome');
@@ -11,3 +13,16 @@ require __DIR__.'/resources/page.php';
 Route::group(['middleware' => ['auth:sanctum', 'json']], function () {
     require __DIR__.'/resources/user.php';
 });
+
+if (Jetstream::hasTermsAndPrivacyPolicyFeature()) {
+    Route::get('/terms-of-service', function () {
+        $record = Page::find(config('page.terms'));
+
+        return view('terms-of-service', ['record' => $record]);
+    })->name('terms.show');
+    Route::get('/privacy-policy', function () {
+        $record = Page::find(config('page.privacy'));
+
+        return view('privacy-policy', ['record' => $record]);
+    })->name('policy.show');
+}
