0853218107161f9f31063e97c0603eb7d26e6d7a
PT Spekta Karya Raya <spektakaryaraya@gmail.com>
2025-04-03 15:54:08 +0700

    feat: implement SEO for page view
    This commit introduces Search Engine Optimization (SEO) functionality to the page view.
    
    It utilizes the `artesaos/seotools` package to dynamically generate meta tags, Open Graph tags, Twitter card tags, and JSON-LD structured data based on the content of each page.
    
    - Updates the `ViewPage` Livewire component to generate SEO tags using data from the page
      being viewed.
    - Extracts a short description from the page content and sets it as the SEO description.
    - Sets SEO title to the page title.
    - Publishes the generated SEO data into the main layout view.
    - Sets default SEO title as the `app.name` config value.

--- config/seotools.php
+++ config/seotools.php
@@ -1,33 +1,34 @@
 <?php
+
 /**
  * @see https://github.com/artesaos/seotools
  */
 
 return [
-    'inertia' => env('SEO_TOOLS_INERTIA', false),
+    'inertia' => false,
     'meta' => [
         /*
          * The default configurations to be used by the meta generator.
          */
-        'defaults'       => [
-            'title'        => "It's Over 9000!", // set false to total remove
-            'titleBefore'  => false, // Put defaults.title before page title, like 'It's Over 9000! - Dashboard'
-            'description'  => 'For those who helped create the Genki Dama', // set false to total remove
-            'separator'    => ' - ',
-            'keywords'     => [],
-            'canonical'    => false, // Set to null or 'full' to use Url::full(), set to 'current' to use Url::current(), set false to total remove
-            'robots'       => false, // Set to 'all', 'none' or any combination of index/noindex and follow/nofollow
+        'defaults' => [
+            'title' => config('app.name', "It's Over 9000!"), // set false to total remove
+            'titleBefore' => false, // Put defaults.title before page title, like 'It's Over 9000! - Dashboard'
+            'description' => 'For those who helped create the Genki Dama', // set false to total remove
+            'separator' => ' - ',
+            'keywords' => [],
+            'canonical' => false, // Set to null or 'full' to use Url::full(), set to 'current' to use Url::current(), set false to total remove
+            'robots' => false, // Set to 'all', 'none' or any combination of index/noindex and follow/nofollow
         ],
         /*
          * Webmaster tags are always added.
          */
         'webmaster_tags' => [
-            'google'    => null,
-            'bing'      => null,
-            'alexa'     => null,
+            'google' => null,
+            'bing' => null,
+            'alexa' => null,
             'pinterest' => null,
-            'yandex'    => null,
-            'norton'    => null,
+            'yandex' => null,
+            'norton' => null,
         ],
 
         'add_notranslate_class' => false,
@@ -37,12 +38,12 @@ return [
          * The default configurations to be used by the opengraph generator.
          */
         'defaults' => [
-            'title'       => 'Over 9000 Thousand!', // set false to total remove
+            'title' => config('app.name', 'Over 9000 Thousand!'), // set false to total remove
             'description' => 'For those who helped create the Genki Dama', // set false to total remove
-            'url'         => false, // Set null for using Url::current(), set false to total remove
-            'type'        => false,
-            'site_name'   => false,
-            'images'      => [],
+            'url' => false, // Set null for using Url::current(), set false to total remove
+            'type' => false,
+            'site_name' => false,
+            'images' => [],
         ],
     ],
     'twitter' => [
@@ -50,8 +51,8 @@ return [
          * The default values to be used by the twitter cards generator.
          */
         'defaults' => [
-            //'card'        => 'summary',
-            //'site'        => '@LuizVinicius73',
+            // 'card'        => 'summary',
+            // 'site'        => '@LuizVinicius73',
         ],
     ],
     'json-ld' => [
@@ -59,11 +60,11 @@ return [
          * The default configurations to be used by the json-ld generator.
          */
         'defaults' => [
-            'title'       => 'Over 9000 Thousand!', // set false to total remove
+            'title' => config('app.name', 'Over 9000 Thousand!'), // set false to total remove
             'description' => 'For those who helped create the Genki Dama', // set false to total remove
-            'url'         => false, // Set to null or 'full' to use Url::full(), set to 'current' to use Url::current(), set false to total remove
-            'type'        => 'WebPage',
-            'images'      => [],
+            'url' => false, // Set to null or 'full' to use Url::full(), set to 'current' to use Url::current(), set false to total remove
+            'type' => 'WebPage',
+            'images' => [],
         ],
     ],
 ];
