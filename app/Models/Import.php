5305afe4b874c9ed03ad94524d1ad0a4ad932980
PT Spekta Karya Raya <spektakaryaraya@gmail.com>
2025-06-12 14:35:47 +0700

    feat: add import resource to filament
    This commit introduces the ImportResource to Filament, allowing administrators to manage imports within the Filament admin panel.
    
    The changes include:
    
    - Creation of the `ImportResource` file with CRUD functionality.
    - Creation of the `ListImports` page for listing imports.
    - Addition of `ImportPolicy` for authorization.
    - Translation keys for new labels.

--- app/Models/Import.php
+++ app/Models/Import.php
@@ -5,6 +5,9 @@ namespace App\Models;
 use Filament\Actions\Imports\Models\Import as FilamentImport;
 use Illuminate\Database\Eloquent\Concerns\HasUlids;
 
+/**
+ * @property-read User $user
+ */
 class Import extends FilamentImport
 {
     use HasUlids;
