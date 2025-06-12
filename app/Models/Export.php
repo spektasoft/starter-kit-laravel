aa5175e81e1393be17271168408609185c1a3497
PT Spekta Karya Raya <spektakaryaraya@gmail.com>
2025-06-12 22:24:49 +0700

    test: add factory and test for Export model
    Adds an Export factory for seeding and testing purposes. Also introduces a test case to ensure that the export directory is properly deleted when the Export model is deleted. This ensures proper cleanup of export files and directories and avoids disk space issues.

--- app/Models/Export.php
+++ app/Models/Export.php
@@ -4,12 +4,29 @@ namespace App\Models;
 
 use Filament\Actions\Exports\Models\Export as FilamentExport;
 use Illuminate\Database\Eloquent\Concerns\HasUlids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Support\Carbon;
 
 /**
+ * @property string $user_id
+ * @property ?Carbon $completed_at
+ * @property string $file_disk
+ * @property string|null $file_name
+ * @property string $exporter
+ * @property int $processed_rows
+ * @property int $total_rows
+ * @property int $successful_rows
+ * @property ?Carbon $created_at
+ * @property ?Carbon $updated_at
  * @property-read User $user
+ *
+ * @method static \Database\Factories\ExportFactory factory(...$parameters)
  */
 class Export extends FilamentExport
 {
+    /** @use HasFactory<\Database\Factories\ExportFactory> */
+    use HasFactory;
+
     use HasUlids;
 
     protected static function booted(): void
