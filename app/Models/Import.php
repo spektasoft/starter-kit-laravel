dd76eabb20f3905d1fe1bc6302713c2133e6aefe
PT Spekta Karya Raya <spektakaryaraya@gmail.com>
2025-06-13 00:37:51 +0700

    chore: add factory for Import model
    Adds a factory for the Import model to facilitate testing and seeding. This factory allows for the easy creation of realistic Import model instances with randomized data. Also adds property type declarations and phpdoc to the Import Model.

--- app/Models/Import.php
+++ app/Models/Import.php
@@ -4,11 +4,29 @@ namespace App\Models;
 
 use Filament\Actions\Imports\Models\Import as FilamentImport;
 use Illuminate\Database\Eloquent\Concerns\HasUlids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Support\Carbon;
 
 /**
+ * @property string $id
+ * @property ?Carbon $completed_at
+ * @property string|null $file_name
+ * @property string $file_path
+ * @property string $importer
+ * @property int $processed_rows
+ * @property int $total_rows
+ * @property int $successful_rows
+ * @property string $user_id
+ * @property ?Carbon $created_at
+ * @property ?Carbon $updated_at
  * @property-read User $user
+ *
+ * @method static \Database\Factories\ImportFactory factory(...$parameters)
  */
 class Import extends FilamentImport
 {
+    /** @use HasFactory<\Database\Factories\ImportFactory> */
+    use HasFactory;
+
     use HasUlids;
 }
