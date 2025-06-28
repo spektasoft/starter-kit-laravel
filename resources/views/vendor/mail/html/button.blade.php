5c7de756b40b512bb69429a8ac6b8d808fede2df
PT Spekta Karya Raya <spektakaryaraya@gmail.com>
2025-06-28 09:08:01 +0700

    fix: prevent fatal error with invalid button color
    Addresses a potential fatal error in `resources/views/vendor/mail/html/button.blade.php` when an invalid color is passed to the mail button component.
    
    Previously, `FilamentColor::getColors()[$color][500]` would cause a fatal error if `$color` was not a valid key in the Filament color array, preventing emails from being sent.
    
    This commit introduces a check using `array_key_exists` to verify if the provided `$color` exists in the Filament color configuration. If it doesn't, the button now gracefully falls back to the `primary` color, ensuring email delivery even with misconfigured button colors.

--- resources/views/vendor/mail/html/button.blade.php
+++ resources/views/vendor/mail/html/button.blade.php
@@ -2,6 +2,8 @@
 
 @php
     use Filament\Support\Facades\FilamentColor;
+    $colors = FilamentColor::getColors();
+    $buttonColor = array_key_exists($color, $colors) ? $colors[$color][500] : $colors['primary'][500];
 @endphp
 
 <table class="action" align="{{ $align }}" width="100%" cellpadding="0" cellspacing="0" role="presentation">
@@ -13,7 +15,7 @@
                         <table border="0" cellpadding="0" cellspacing="0" role="presentation">
                             <tr>
                                 <td
-                                    style="border-radius: 8px; background-color: rgb({{ FilamentColor::getColors()[$color][500] }}); padding: 0.5rem 0.75rem;">
+                                    style="border-radius: 8px; background-color: rgb({{ $buttonColor }}); padding: 0.5rem 0.75rem;">
                                     <a href="{{ $url }}" class="button" target="_blank" rel="noopener"
                                         style="background-color: transparent; display: inline-block;">{!! $slot !!}</a>
                                 </td>
